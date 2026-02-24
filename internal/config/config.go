ea7de11e4a03ea7468adb62834ffad02f1b6fc7b
pyw0w <myxi2002@gmail.com>
2026-02-24 17:05:41 +0500

    chore: Install dependencies and update page transition styling and quota constants.

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	DefaultPanelGitHubRepository = "https://github.com/router-for-me/Cli-Proxy-API-Management-Center"
+	DefaultPanelGitHubRepository = "https://github.com/pyw0w/Cli-Proxy-API-Management-Center"
 	DefaultPprofAddr             = "127.0.0.1:8316"
 )
 
